eccfc0193cc4a3e568637b9c446d3bc7bbc6ff1c
Klim <Severin.Klim@yandex.by>
2021-05-28 23:32:36 +0300

    Map

---
+++ Advanced/Problem 5 - Map/Map.js
@@ -0,0 +1,8 @@
+// built-in Array.prototype.map
+function map(array, callback) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i]));
+    }
+    return result;
+}
